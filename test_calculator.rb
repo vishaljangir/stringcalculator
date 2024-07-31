f5b2674bd2d3c98c9c5b6530d8084ac5bcf91016
Vishal Jangir <vishaljangir022@gmail.com>
2024-07-31 20:12:29 +0530

    test case with one

--- test_calculator.rb
+++ test_calculator.rb
@@ -9,4 +9,8 @@ class TestCalculator < Minitest::Test
   def test_check_with_empty_string
     assert_equal 0, @calculator.sum_only_integer_values("")
   end
+
+  def test_check_with_one
+    assert_equal 1, @calculator.sum_only_integer_values("1")
+  end
 end
