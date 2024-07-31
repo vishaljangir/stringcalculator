082981812996c5ab3617c9228bb6d18e542bc88f
Vishal Jangir <vishaljangir022@gmail.com>
2024-07-31 20:24:08 +0530

    handle new line in between string

--- test_calculator.rb
+++ test_calculator.rb
@@ -17,4 +17,8 @@ class TestCalculator < Minitest::Test
   def test_with_multiple
     assert_equal 5, @calculator.sum_only_integer_values("1,2,2")
   end
+
+  def test_handle_new_line
+    assert_equal 6, @calculator.sum_only_integer_values("1\n2, 3")
+  end
 end
